751452253e1a0b1c26e0b176c1f53b4393d84b89
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-12-15 11:40:40 -0300

    E-mail duplicado

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Osvaldo Kalvaitir Filho',
+        email: 'osvaldokalvaitir@outlook.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Osvaldo Kalvaitir Filho',
+        email: 'osvaldokalvaitir@outlook.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
